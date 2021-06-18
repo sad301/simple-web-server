dd2e04a9720b516a33e9a8fa4af8ab6096b507bb
sad301 <sad301@gmail.com>
2021-06-19 03:19:42 +0800

    from idea-ce

--- src/main/java/com/sad301/webserver/App.java
+++ src/main/java/com/sad301/webserver/App.java
@@ -1,7 +1,14 @@
 package com.sad301.webserver;
 
+import javax.print.event.PrintJobAttributeListener;
 import javax.swing.*;
 import spark.*;
+
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.GroupPrincipal;
 
 /**
 * Hello world!
@@ -19,10 +26,12 @@ public class App extends JFrame {
     catch(Exception exc) {
       exc.printStackTrace();
     }
-    SwingUtilities.invokeLater(() -> {
-      App app = new App();
-      app.setVisible(true);
-    });
+    finally {
+      SwingUtilities.invokeLater(() -> {
+        App app = new App();
+        app.setVisible(true);
+      });
+    }
   }
 
   public App() {
@@ -30,55 +39,77 @@ public class App extends JFrame {
   }
 
   private void createGUI() {
-    JLabel lRoot = new JLabel("Root Directory :");
+    JLabel lAddress = new JLabel("Address :");
+    JTextField tfAddress = new JTextField("127.0.0.1");
+
+    JLabel lPort = new JLabel("Port :");
+    JTextField tfPort = new JTextField("8000");
+
+    JLabel lRoot = new JLabel("Directory :");
     JTextField tfRoot = new JTextField();
-    JButton btnExec = new JButton("Start");
-    btnExec.addActionListener(e -> {
-      switch(e.getActionCommand()) {
-        case "Start" -> {
-          btnExec.setActionCommand("Stop");
-          String root = tfRoot.getText();
-          service = Service.ignite()
-            .externalStaticFileLocation(root)
-            .port(8000);
-          service.init();
-        }
-        case "Stop" -> {
-          btnExec.setActionCommand("Start");
-          service.stop();
+    tfRoot.addFocusListener(new FocusListener() {
+      boolean showing = false;
+      @Override
+      public void focusGained(FocusEvent e) {
+        System.out.println("gained");
+        if(!showing) {
+          showing = true;
+          JFileChooser fc=  new JFileChooser();
+          fc.showOpenDialog(null);
         }
       }
-      btnExec.setText(btnExec.getActionCommand());
+      @Override
+      public void focusLost(FocusEvent e) {
+        System.out.println("lost");
+        showing = false;
+      }
     });
-    JTextArea taLog = new JTextArea();
-    JScrollPane spLog = new JScrollPane(taLog);
+
+    JButton btnExec = new JButton("Start");
 
     GroupLayout gl = new GroupLayout(getContentPane());
     gl.setAutoCreateGaps(true);
     gl.setAutoCreateContainerGaps(true);
     gl.setHorizontalGroup(gl.createSequentialGroup()
-      .addGroup(gl.createParallelGroup()
-        .addGroup(gl.createSequentialGroup()
-          .addComponent(lRoot)
-          .addComponent(tfRoot)
-          .addComponent(btnExec)
-        )
-        .addComponent(spLog)
-      )
+            .addGroup(gl.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                    .addGroup(gl.createSequentialGroup()
+                            .addGroup(gl.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                                    .addComponent(lAddress)
+                                    .addComponent(lPort)
+                                    .addComponent(lRoot)
+                            )
+                            .addGroup(gl.createParallelGroup()
+                                    .addComponent(tfAddress)
+                                    .addComponent(tfPort)
+                                    .addComponent(tfRoot)
+                            )
+                    )
+                    .addComponent(btnExec)
+            )
     );
     gl.setVerticalGroup(gl.createSequentialGroup()
-      .addGroup(gl.createParallelGroup(GroupLayout.Alignment.BASELINE)
-        .addComponent(lRoot)
-        .addComponent(tfRoot)
-        .addComponent(btnExec)
-      )
-      .addComponent(spLog)
+            .addGroup(gl.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(lAddress)
+                    .addComponent(tfAddress)
+            )
+            .addGroup(gl.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(lPort)
+                    .addComponent(tfPort)
+            )
+            .addGroup(gl.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(lRoot)
+                    .addComponent(tfRoot)
+            )
+            .addComponent(btnExec)
     );
 
     getContentPane().setLayout(gl);
 
+    pack();
+    setSize(250, getHeight());
+    setResizable(false);
+
     setTitle("Simple Web Server");
-    setSize(400, 400);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setLocationRelativeTo(null);
   }
